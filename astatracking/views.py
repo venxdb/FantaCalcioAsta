ccd1f1b289c55b829110b1b5be282e2fd3237a3a
zanteste <st.zantedeschi@gmail.com>
2024-08-12 21:16:38 +0200

    nome utente mostrato nella pagina del menu

--- astatracking/views.py
+++ astatracking/views.py
@@ -1,12 +1,13 @@
 # views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
+        user     = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('menu')  # Redirige a 'menu' se l'autenticazione ha successo
@@ -15,8 +16,14 @@ def login_view(request):
             return render(request, 'login.html', {'error': 'Credenziali errate'})
     return render(request, 'login.html')
 
+@login_required
 def menu_view(request):
-    return render(request, 'menu.html')
+
+    user    = request.user
+    context = {
+        'user_name': user.first_name
+    }
+    return render(request, 'menu.html', context)
 
 def index_view(request):
     return redirect('login')  # Reindirizza alla pagina di login dalla radice
