de227dbbbe5c0a9fb5f76a8fc348c21dde478f14
zanteste <st.zantedeschi@gmail.com>
2024-08-18 16:31:13 +0200

    settings.py cambiato

--- fantaasta/settings.py
+++ fantaasta/settings.py
@@ -27,9 +27,11 @@ SECRET_KEY = 'django-insecure-(z_89zk_=#bxrdwg0fcf^2q%#^tkd-_786)f&p_pu#wnz2bmuv
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = []
 
+# ALLOWED_HOSTS = ['https://fantacorrubio.pythonanywhere.com/']
 
 # Application definition
 
@@ -122,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
